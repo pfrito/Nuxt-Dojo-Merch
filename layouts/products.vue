3c99e2e41630d4a30b4f33de3953c446a8126a71
pfrito <carlos.rolando.morell@gmail.com>
2024-03-09 13:27:35 -0500

    products card added

--- layouts/products.vue
+++ layouts/products.vue
@@ -10,7 +10,7 @@
       <slot />
     </div>
 
-    <footer class="container mx-auo p-4 flex justify-between border-t-2">
+    <footer class="container mx-auto p-4 flex justify-between border-t-2">
       <ul class="flex gap-4">
         <li><NuxtLink to="/">Home</NuxtLink></li>
         <li><NuxtLink to="/about">About</NuxtLink></li>
